38c276efea610a661f04d2c67010401ae12dec1d
Anastasia250722 <125464710+Anastasia250722@users.noreply.github.com>
2023-03-15 09:16:04 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
 URL_SERVICE = 'https://45d4fefd-9d1c-4d83-9d9a-d07c0e43e91f.serverhub.praktikum-services.ru'
 CREATE_USER_ORDERS = '/api/v1/orders'
-GET_ORDER_TRACK="/api/v1/orders/track"
+GET_ORDER_TRACK='/v1/orders/track?t=892489'
